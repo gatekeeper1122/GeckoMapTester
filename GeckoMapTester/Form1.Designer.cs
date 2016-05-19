0dccaa3c11ab577b8ebb8273ffcc2c48f9173653
jc <fakecake@icloud.com>
2016-05-19 15:40:23 +0200

    Initial commit

---
+++ GeckoMapTester/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace GeckoMapTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.IPBox = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.NameCBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DisconnButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.PokeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // IPBox
+            // 
+            this.IPBox.Location = new System.Drawing.Point(6, 19);
+            this.IPBox.Name = "IPBox";
+            this.IPBox.Size = new System.Drawing.Size(100, 20);
+            this.IPBox.TabIndex = 0;
+            this.IPBox.Text = "192.168.178.22";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(112, 17);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(112, 23);
+            this.ConnectButton.TabIndex = 1;
+            this.ConnectButton.Text = "Connect to Gecko";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NameCBox
+            // 
+            this.NameCBox.FormattingEnabled = true;
+            this.NameCBox.Items.AddRange(new object[] {
+            "Fld_Crank00_Vss",
+            "Fld_Warehouse00_Vss",
+            "Fld_SeaPlant00_Vss",
+            "Fld_UpDown00_Vss",
+            "Fld_SkatePark00_Vss",
+            "Fld_Athletic00_Vss",
+            "Fld_Amida00_Vss",
+            "Fld_Maze00_Vss",
+            "Fld_Tuzura00_Vss",
+            "Fld_Ruins00_Vss",
+            "Fld_ShootingRange_Shr",
+            "Fld_Office00_Vss",
+            "Fld_Quarry00_Vss",
+            "Fld_Jyoheki00_Vss",
+            "Fld_Pivot00_Vss",
+            "Fld_Hiagari00_Vss",
+            "Fld_Kaisou00_Vss",
+            "Fld_Crank00_Dul",
+            "Fld_Warehouse00_Dul",
+            "Fld_SeaPlant00_Dul",
+            "Fld_UpDown00_Dul",
+            "Fld_SkatePark00_Dul",
+            "Fld_Tutorial00_Ttr",
+            "Fld_TutorialShow00_Ttr",
+            "Fld_World00_Wld",
+            "Fld_EasyHide00_Msn",
+            "Fld_EasyClimb00_Msn",
+            "Fld_EasyJump00_Msn",
+            "Fld_Geyser00_Msn",
+            "Fld_Sponge00_Msn",
+            "Fld_Propeller00_Msn",
+            "Fld_PaintingLift00_Msn",
+            "Fld_RvlMaze00_Msn",
+            "Fld_OctZero00_Msn",
+            "Fld_InkRail00_Msn",
+            "Fld_Invisible00_Msn",
+            "Fld_Dozer00_Msn",
+            "Fld_SlideLift00_Msn",
+            "Fld_RvlSkatePark00_Msn",
+            "Fld_OctRuins00_Msn",
+            "Fld_Propeller01_Msn",
+            "Fld_Charge00_Msn",
+            "Fld_PaintingLift01_Msn",
+            "Fld_TurnLift00_Msn",
+            "Fld_RvlRuins00_Msn",
+            "Fld_OctCrank00_Msn",
+            "Fld_Trance00_Msn",
+            "Fld_Sponge01_Msn",
+            "Fld_Fusya00_Msn",
+            "Fld_Dozer01_Msn",
+            "Fld_RvlSeaPlant00_Msn",
+            "Fld_OctSkatePark00_Msn",
+            "Fld_BossStampKing_Bos_Msn",
+            "Fld_BossCylinderKing_Bos_Msn",
+            "Fld_BossBallKing_Bos_Msn",
+            "Fld_BossMouthKing_Bos_Msn",
+            "Fld_BossRailKing_Bos_Msn"});
+            this.NameCBox.Location = new System.Drawing.Point(6, 40);
+            this.NameCBox.Name = "NameCBox";
+            this.NameCBox.Size = new System.Drawing.Size(257, 21);
+            this.NameCBox.TabIndex = 2;
+            this.NameCBox.Text = "Fld_BossMouthKing_Bos_Msn";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DisconnButton);
+            this.groupBox1.Controls.Add(this.ConnectButton);
+            this.groupBox1.Controls.Add(this.IPBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 49);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TCPGecko Connection";
+            // 
+            // DisconnButton
+            // 
+            this.DisconnButton.Enabled = false;
+            this.DisconnButton.Location = new System.Drawing.Point(230, 17);
+            this.DisconnButton.Name = "DisconnButton";
+            this.DisconnButton.Size = new System.Drawing.Size(112, 23);
+            this.DisconnButton.TabIndex = 2;
+            this.DisconnButton.Text = "Disconnect";
+            this.DisconnButton.UseVisualStyleBackColor = true;
+            this.DisconnButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.PokeButton);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.NameCBox);
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(12, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 76);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Map";
+            // 
+            // PokeButton
+            // 
+            this.PokeButton.Location = new System.Drawing.Point(269, 38);
+            this.PokeButton.Name = "PokeButton";
+            this.PokeButton.Size = new System.Drawing.Size(75, 23);
+            this.PokeButton.TabIndex = 11;
+            this.PokeButton.Text = "Poke";
+            this.PokeButton.UseVisualStyleBackColor = true;
+            this.PokeButton.Click += new System.EventHandler(this.PokeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(15, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Only for Splatoon 2.7.0, thanks to NWPlayer and CFLean for helpful stuff";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 165);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "GeckoTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IPBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.ComboBox NameCBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button PokeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DisconnButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ GeckoMapTester/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GeckoMapTester
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public TCPGecko Gecko;
+        string MapAscii;
+        uint address;
+        string extrasite;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] arguments = Environment.GetCommandLineArgs();
+            try
+            {
+                extrasite = arguments[1];
+                MessageBox.Show("Using custom python library dir: \n\n" + extrasite, "", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+            }
+            catch (Exception exc)
+            {
+                extrasite = @"C:\Python27\Lib";
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Gecko = new TCPGecko(IPBox.Text, 7331);
+            try {
+                Gecko.Connect();
+            }catch(ETCPGeckoException exc)
+            {
+                MessageBox.Show("Connection to the TCPGecko failed: \n\n" + exc.Message, "Connection failed", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            groupBox2.Enabled = true;
+            DisconnButton.Enabled = true;
+            ConnectButton.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Gecko.Disconnect();
+            groupBox2.Enabled = false;
+            DisconnButton.Enabled = false;
+            ConnectButton.Enabled = true;
+        }
+
+        /* hacky, needs ironpython and messes up padding
+        private void writestr(uint address, string str)
+        {
+            ScriptEngine engine = Python.CreateEngine();
+            try {
+                ICollection<string> paths = engine.GetSearchPaths();
+
+
+                paths.Add(extrasite);
+                //paths.Add(Environment.CurrentDirectory);
+
+                engine.SetSearchPaths(paths);
+
+                var script = @"import struct
+if type(string) != bytes: string = bytes(string, ""UTF - 8"")
+if len(string) % 4: string += bytes((4 - (len(string) % 4)) * b""\x00"")
+pos = 0
+for x in range(int(len(string) / 4)):
+    me.PokeDummy(address, struct.unpack("">I"", string[pos:pos + 4])[0])
+    address += 4;
+    pos += 4";
+
+                ScriptScope scope = engine.CreateScope();
+
+                //Test class with a method that prints to the screen.
+                scope.SetVariable("me", this);
+                scope.SetVariable("string", str);
+                scope.SetVariable("address", address);
+
+                ScriptSource source = engine.CreateScriptSourceFromString(script, Microsoft.Scripting.SourceCodeKind.Statements);
+
+
+                source.Execute(scope);
+
+            }catch(Exception exc)
+            {
+                MessageBox.Show("IronPython Operation failed: \n\n" + engine.GetService<ExceptionOperations>().FormatException(exc) + "\n\nCheck your lib directory!", "Operation failed", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+        */
+
+        private void PokeAllMaps(string NewMapName)
+        {
+            writeStringSimple(0x12AEE594, NewMapName);
+            writeStringSimple(0x12AEE51C, NewMapName);
+            writeStringSimple(0x12AEE4A4, NewMapName);
+            writeStringSimple(0x12AEE42C, NewMapName);
+            writeStringSimple(0x12AEE3B4, NewMapName);
+            writeStringSimple(0x12AEE33C, NewMapName);
+            writeStringSimple(0x12AEE2C4, NewMapName);
+            writeStringSimple(0x12AEE24C, NewMapName);
+            writeStringSimple(0x12AEE1D4, NewMapName);
+            writeStringSimple(0x12AEE15C, NewMapName);
+            writeStringSimple(0x12AEE0E4, NewMapName);
+            writeStringSimple(0x12AEE06C, NewMapName);
+            writeStringSimple(0x12AEDFF4, NewMapName);
+            writeStringSimple(0x12AEDF7C, NewMapName);
+            writeStringSimple(0x12AEDF04, NewMapName);
+            writeStringSimple(0x12AEDE8C, NewMapName);
+
+            writeStringSimple(0x12B4BA3C, NewMapName);
+            writeStringSimple(0x12B4BCC4, NewMapName);
+            writeStringSimple(0x12B4BF98, NewMapName);
+            writeStringSimple(0x12B4C1D4, NewMapName);
+            writeStringSimple(0x12B4C45C, NewMapName);
+            writeStringSimple(0x12B4C6E4, NewMapName);
+            writeStringSimple(0x12B4C96C, NewMapName);
+            writeStringSimple(0x12B4CBF4, NewMapName);
+            writeStringSimple(0x12B4CE7C, NewMapName);
+            writeStringSimple(0x12B4D104, NewMapName);
+            writeStringSimple(0x12B4D38C, NewMapName);
+            writeStringSimple(0x12B4D614, NewMapName);
+        }
+
+        private void PokeButton_Click(object sender, EventArgs e)
+        {
+            PokeAllMaps(NameCBox.Text);
+            MessageBox.Show("Success!", "GeckoTool", MessageBoxButtons.OK);
+        }
+
+        /* gah
+        public void PokeDummy(int add, int val)
+        {
+
+            //MessageBox.Show("add: " + add + "\nval: " + val, "", MessageBoxButtons.OK, MessageBoxIcon.Stop);        
+            try {
+                uint test1 = Convert.ToUInt32(add);
+                uint test2 = Convert.ToUInt32(val);
+
+                try
+                {
+                    Gecko.poke(test1, test2);
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Writing values to RAM failed: \n\n" + exc.Message, "Poke failed", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show("Writing values to RAM failed: \n\n" + exc.Message, "Poke failed", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+        */
+        private void writeString(string s, uint offset)
+        {
+            uint push = 0;
+            int pos = 0;
+            if (offset % 4 != 0)
+            {
+                push = s[pos++];
+                offset -= offset % 4;
+                Gecko.poke(offset, push);
+                offset += 4;
+            }
+            for (; pos < s.Length; pos += 2, offset += 4)
+            {
+                if (pos + 1 == s.Length)
+                {
+                    push = (uint)(s[pos] << 16) + (Gecko.peek(offset) & 0xFF);
+                }
+                push = (uint)(s[pos] << 16) + s[pos + 1];
+                Gecko.poke(offset, push);
+            }
+        }
+
+        private void writeStringSimple(uint offset, string s)
+        {
+            writeStringSimple(offset, s, s.Length);
+        }
+        private void writeStringSimple(uint offset, string s, int length)
+        {
+            uint push = 0;
+            int pos = 0;
+            if (offset % 4 != 0)
+            {
+                for (int i = 0; i < offset % 4; i++)
+                {
+                    push = push << 8 | s[pos++];
+                }
+                if (offset % 4 == 1)
+                {
+                    push = Gecko.peek(offset - offset % 4) & 0xFF000000 | push;
+                }
+                if (offset % 4 == 2)
+                {
+                    push = Gecko.peek(offset - offset % 4) & 0xFFFF0000 | push;
+                }
+                if (offset % 4 == 3)
+                {
+                    push = Gecko.peek(offset - offset % 4) & 0xFFFFFF00 | push;
+                }
+                Gecko.poke(offset, push);
+                offset += offset % 4;
+            }
+            for (; pos < s.Length; offset += 4)
+            {
+                push = 0;
+                if (pos + 1 == s.Length)
+                {
+                    push = (uint)s[pos++] << 24 | Gecko.peek(offset) & 0x00FFFFFF;
+                    Gecko.poke(offset, push);
+                    offset += 1;
+                    break;
+                }
+                if (pos + 2 == s.Length)
+                {
+                    push = s[pos++];
+                    push = push << 8 | s[pos++];
+                    push = push << 16 | Gecko.peek(offset) & 0x0000FFFF;
+                    Gecko.poke(offset, push);
+                    offset += 2;
+                    break;
+                }
+                if (pos + 3 == s.Length)
+                {
+                    push = s[pos++];
+                    push = push << 8 | s[pos++];
+                    push = push << 8 | s[pos++];
+                    push = push << 8 | Gecko.peek(offset) & 0x000000FF;
+                    Gecko.poke(offset, push);
+                    break;
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    push = push << 8 | s[pos++];
+                }
+                Gecko.poke(offset, push);
+            }
+            for (; pos < length; offset += 4, pos += 4)
+            {
+                if (pos % 4 == 1)
+                {
+                    Gecko.poke(offset, Gecko.peek(offset) & 0xFF000000);
+                    pos--;
+                    continue;
+                }
+                if (pos % 4 == 2)
+                {
+                    Gecko.poke(offset, Gecko.peek(offset) & 0xFFFF0000);
+                    pos--; pos--;
+                    continue;
+                }
+                if (pos % 4 == 3)
+                {
+                    Gecko.poke(offset, Gecko.peek(offset) & 0xFFFFFF00);
+                    pos--; pos--; pos--;
+                    continue;
+                }
+                if (pos + 1 == length)
+                {
+                    push = Gecko.peek(offset) & 0x00FFFFFF;
+                    Gecko.poke(offset, push);
+                    offset += 1;
+                    pos++;
+                    break;
+                }
+                if (pos + 2 == length)
+                {
+                    push = Gecko.peek(offset) & 0x0000FFFF;
+                    Gecko.poke(offset, push);
+                    offset += 2;
+                    pos += 2;
+                    break;
+                }
+                if (pos + 3 == length)
+                {
+                    push = Gecko.peek(offset) & 0x000000FF;
+                    Gecko.poke(offset, push);
+                    offset += 3;
+                    pos += 3;
+                    break;
+                }
+                Gecko.poke(offset, 0);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
